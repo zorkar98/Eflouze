25fa21109b09fa04923203f4f46ddc213e5a78a7
clongein2021 <clongein2021@12SE50-CKLRZS2.ad.campus-eni.fr>
2022-02-15 13:55:51 +0100

    modification User_Manager : -ajout verifierUser()

--- src/main/java/eFlouz/bll/User_manager.java
+++ src/main/java/eFlouz/bll/User_manager.java
@@ -9,6 +9,7 @@ import eFlouz.dal.userDAOJDBCImpl;
 public class User_manager extends userDAOJDBCImpl{
 	
 	private List<User> listeUser;
+
 	
 	public User_manager() {
 		super();
@@ -24,9 +25,23 @@ public class User_manager extends userDAOJDBCImpl{
 		return listeUser;
 	}
 		
+	public boolean verifierUser(String email, String mot_de_passe) {
+		boolean validation = false;
+			
+		for (User utilisateur : listeUser) {
+			String courriel = utilisateur.getEmail();
+			String mdp = utilisateur.getMot_de_passe();
+			
+			if (courriel == email && mdp == mot_de_passe) {
+				validation = true;
+			}
+		}
+		return validation;
+	}
 	
 	public void ajouterUser(String email, String mot_de_passe) {
 		User user = new User(email, mot_de_passe);
+		
 	}
 
 }
