8fcf29639069172fa7f1c2aa14cb5df834e62e0f
clongein2021 <clongein2021@12SE50-CKLRZS2.ad.campus-eni.fr>
2022-02-17 16:41:34 +0100

    ajout AccueillirServlet + accueilJSP + styleCSS

---
+++ src/main/java/eFlouz/ihm/AccueillirServlet.java
@@ -0,0 +1,46 @@
+package eFlouz.ihm;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AccueillirServlet
+ */
+@WebServlet("/home")
+public class AccueillirServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccueillirServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+		if (rd != null) {
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		doGet(request, response);
+	}
+
+}
