74927a52f5f2ed197e109c67542e9fcbb82db68a
clongein2021 <clongein2021@12SE50-CKLRZS2.ad.campus-eni.fr>
2022-02-21 15:09:36 +0100

    Redirection VendreServlet

--- src/main/java/eFlouz/ihm/VendreServlet.java
+++ src/main/java/eFlouz/ihm/VendreServlet.java
@@ -36,7 +36,13 @@ public class VendreServlet extends HttpServlet {
 		if (rd != null) {
 			rd.forward(request, response);
 		}
-		HttpSession session = request.getSession();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+HttpSession session = request.getSession();
 		
 		String nomArticle = request.getParameter("nomArticle");
 		String description = request.getParameter("description");
@@ -58,17 +64,7 @@ public class VendreServlet extends HttpServlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if (rd != null) {
-			rd.forward(request, response);
-		}
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
