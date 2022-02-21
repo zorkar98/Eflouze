f9cada5de283b7b44028c31e5ed7571759fa0500
Clément Quéré <zorkar98@hotmail.fr>
2022-02-21 11:28:13 +0100

    modif indentation ArticleDAOJBDCImpl, Article, ArticleManager

--- src/main/java/eFlouz/bll/ArticleManager.java
+++ src/main/java/eFlouz/bll/ArticleManager.java
@@ -6,23 +6,19 @@ import eFlouz.bo.Article;
 import eFlouz.dal.ArticleDAOJBDCImpl;
 
 public class ArticleManager {
-	
+
 	ArticleDAOJBDCImpl articleDao = new ArticleDAOJBDCImpl();
 
-	
-	//Fonction de création d'une nouvelle vente
-	public void ajouterArticle ( int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres, int prixInitial, int noUtilisateur, int noCategorie) throws Exception {
-		
-		Article articleAAjouter = new Article (noArticle, nomArticle, description, dateDebutEncheres, dateFinEncheres, prixInitial, noUtilisateur, noCategorie);
-		
+	// Fonction de création d'une nouvelle vente
+	public void ajouterArticle(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int prixInitial, int noUtilisateur, int noCategorie) throws Exception {
+
+		Article articleAAjouter = new Article(noArticle, nomArticle, description, dateDebutEncheres, dateFinEncheres,
+				prixInitial, noUtilisateur, noCategorie);
+
 		ArticleDAOJBDCImpl articleDAOJBDCImpl = new ArticleDAOJBDCImpl();
-			articleDAOJBDCImpl.insertArticle(articleAAjouter);
+		articleDAOJBDCImpl.insertArticle(articleAAjouter);
 
 	}
-	
-	
-	
-	
-	
-	
+
 }
--- src/main/java/eFlouz/bo/Article.java
+++ src/main/java/eFlouz/bo/Article.java
@@ -3,13 +3,15 @@ package eFlouz.bo;
 import java.time.LocalDate;
 
 public class Article {
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	private int noArticle;
 	private String nomArticle;
 	private String description;
@@ -19,11 +21,9 @@ public class Article {
 	private int prixVente;
 	private int noUtilisateur;
 	private int noCategorie;
-	
-	
-	
-	public Article(int noArticle, String nomArticle, String description, LocalDate dateDebutEnchere, LocalDate dateFinEnchere,
-			int prixInitial, int prixVente, int noUtilisateur, int noCategorie) {
+
+	public Article(int noArticle, String nomArticle, String description, LocalDate dateDebutEnchere,
+			LocalDate dateFinEnchere, int prixInitial, int prixVente, int noUtilisateur, int noCategorie) {
 		super();
 		this.noArticle = noArticle;
 		this.nomArticle = nomArticle;
@@ -34,55 +34,72 @@ public class Article {
 		this.noUtilisateur = noUtilisateur;
 		this.noCategorie = noCategorie;
 	}
+
 	public Article(int noArticle2, String nomArticle2, String description2, LocalDate dateDebutEncheres,
 			LocalDate dateFinEncheres, int prixInitial2, int noUtilisateur2, int noCategorie2) {
 		// TODO Auto-generated constructor stub
 	}
+
 	public int getNoArticle() {
 		return noArticle;
 	}
+
 	public void setNoArticle(int noArticle) {
 		this.noArticle = noArticle;
 	}
+
 	public String getNomArticle() {
 		return nomArticle;
 	}
+
 	public void setNomArticle(String nomArticle) {
 		this.nomArticle = nomArticle;
 	}
+
 	public LocalDate getDateDebutEnchere() {
 		return dateDebutEnchere;
 	}
+
 	public void setDateDebutEnchere(LocalDate dateDebutEnchere) {
 		this.dateDebutEnchere = dateDebutEnchere;
 	}
+
 	public LocalDate getDateFinEnchere() {
 		return dateFinEnchere;
 	}
+
 	public void setDateFinEnchere(LocalDate dateFinEnchere) {
 		this.dateFinEnchere = dateFinEnchere;
 	}
+
 	public int getPrixInitial() {
 		return prixInitial;
 	}
+
 	public void setPrixInitial(int prixInitial) {
 		this.prixInitial = prixInitial;
 	}
+
 	public int getPrixVente() {
 		return prixVente;
 	}
+
 	public void setPrixVente(int prixVente) {
 		this.prixVente = prixVente;
 	}
+
 	public int getNoUtilisateur() {
 		return noUtilisateur;
 	}
+
 	public void setNoUtilisateur(int noUtilisateur) {
 		this.noUtilisateur = noUtilisateur;
 	}
+
 	public int getNoCategorie() {
 		return noCategorie;
 	}
+
 	public void setNoCategorie(int noCategorie) {
 		this.noCategorie = noCategorie;
 	}
